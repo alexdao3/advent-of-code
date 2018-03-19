844b6ad335ac2a4b026f70f7cc57dca4ce749b28
Alex Dao <alex@urbint.com>
2018-03-19 01:07:47 -0400

    feat: day 09, part2

--- 2017/day09/app/Main.hs
+++ 2017/day09/app/Main.hs
@@ -6,8 +6,9 @@ type Count = Int
 type Multiplier = Int
 type Stack = String
 type NextChar = Char
+type GarbageCount = Int
 data State = OpenGroup | OpenGarbage | Negate deriving Eq
-data NextState = NextState String Count Multiplier
+data NextState = NextState String Count Multiplier GarbageCount
 
 deriveState :: Char -> State
 deriveState '{' = OpenGroup
@@ -15,37 +16,37 @@ deriveState '<' = OpenGarbage
 deriveState '!' = Negate
 
 handleOpenGroup :: Char -> NextState -> NextState
-handleOpenGroup '<' (NextState stack count multiplier) =
-  NextState (stack ++ "<") count multiplier
-handleOpenGroup '!' (NextState stack count multiplier) =
-  NextState (stack ++ "!") count multiplier
-handleOpenGroup '{' (NextState stack count multiplier) =
-  NextState (stack ++ "{") count (multiplier + 1)
-handleOpenGroup '}' (NextState stack count multiplier) =
-  NextState (init stack) (multiplier + count) (multiplier - 1)
+handleOpenGroup '<' (NextState stack count multiplier gc) =
+  NextState (stack ++ "<") count multiplier gc
+handleOpenGroup '!' (NextState stack count multiplier gc) =
+  NextState (stack ++ "!") count multiplier gc
+handleOpenGroup '{' (NextState stack count multiplier gc) =
+  NextState (stack ++ "{") count (multiplier + 1) gc
+handleOpenGroup '}' (NextState stack count multiplier gc) =
+  NextState (init stack) (multiplier + count) (multiplier - 1) gc
 handleOpenGroup _ nextState = nextState
 
 handleGarbage :: Char -> NextState -> NextState
-handleGarbage '!' (NextState stack count multiplier) =
-  NextState (stack ++ "!") count multiplier
-handleGarbage '>' (NextState stack count multiplier) =
-  NextState (init stack) count multiplier
-handleGarbage _ nextState = nextState
+handleGarbage '!' (NextState stack count multiplier gc) =
+  NextState (stack ++ "!") count multiplier gc
+handleGarbage '>' (NextState stack count multiplier gc) =
+  NextState (init stack) count multiplier gc
+handleGarbage _ (NextState stack count multiplier gc) = NextState stack count multiplier $ gc + 1
 
-advance :: String -> Int
+advance :: String -> (Count, GarbageCount)
 advance (x:xs) =
-  advance' xs (NextState "{" 0 1)
+  advance' xs (NextState "{" 0 1 0)
   where
-    advance' [] (NextState _ count _) = count
+    advance' [] (NextState _ count _ gc) = (count, gc)
     advance' (x:xs) nextState = advance' xs $ handleNextState x nextState
 
 handleNextState :: Char -> NextState -> NextState
-handleNextState char (NextState stack count multiplier)
+handleNextState char (NextState stack count multiplier gc)
   -- What is the way to there is bind a variable to a pattern during pattern matching
-  | stack /= "" && (deriveState $ last stack) == OpenGroup     = handleOpenGroup char (NextState stack count multiplier)
-  | stack /= "" && (deriveState $ last stack) == OpenGarbage   = handleGarbage char (NextState stack count multiplier)
-  | stack /= "" && (deriveState $ last stack) == Negate        = NextState (init stack) count multiplier
-  | otherwise                                   = NextState stack count multiplier
+  | stack /= "" && (deriveState $ last stack) == OpenGroup     = handleOpenGroup char (NextState stack count multiplier gc)
+  | stack /= "" && (deriveState $ last stack) == OpenGarbage   = handleGarbage char (NextState stack count multiplier gc)
+  | stack /= "" && (deriveState $ last stack) == Negate        = NextState (init stack) count multiplier gc
+  | otherwise                                                  = NextState stack count multiplier gc
 
 main :: IO ()
 main = do
