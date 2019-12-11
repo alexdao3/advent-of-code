2485b6fdc74ecf7117aed08164ca2df391045276
Alex Dao <rahx1t@gmail.com>
2019-12-10 21:42:43 -0500

    feat: day 06, part 2, 2019

--- 2019/src/day06/Part2.purs
+++ 2019/src/day06/Part2.purs
@@ -3,39 +3,86 @@ module Day06.Part2 where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
-
-import Data.Array (all, any, concat, filter, foldr, length, uncons, zip, (..))
-import Data.HashMap as HM
+import Node.FS.Sync (readTextFile)
+import Node.Encoding (Encoding(UTF8))
+import Data.Array ((:))
+import Data.HashMap as Map
 import Data.Maybe (Maybe(..))
-import Data.Tuple (Tuple(..), fst)
+import Data.Set (Set, fromFoldable, intersection, difference, size)
+import Data.String (split, Pattern(..))
+import Data.Tuple (Tuple(..), fst, snd, swap)
 
-splitNum :: Int -> Array Int-> Array Int
-splitNum x y =
-    let digit = mod x 10
-        remainder = (x - digit) / 10
-    in
-        if x == 0 then y
-        else concat [splitNum remainder y, [digit], y]
+inputFile :: String
+inputFile = "src/day06/input.txt"
 
-isEqualOrIncreasing :: Array Int -> Boolean
-isEqualOrIncreasing xs = case uncons xs of
-            Just {head: y, tail: ys} -> 
-                let pairs = zip xs ys
-                    isAtLeastEqual =  all (\ (Tuple a b) -> a <= b) pairs
-                    consecutiveEqual = map fst $ filter (\ (Tuple a b) -> a == b) pairs
-                    -- can clean this up to use one of the maybe functions that operates on maybe instead of case matching
-                    matchHowMany = foldr (\ k obj -> HM.alter (\ mVal -> case mVal of 
-                        Just val -> Just (val + 1)
-                        Nothing -> Just 1) k obj) HM.empty consecutiveEqual
-                    -- check that there is at least one number with only a single entry indicating a pair
-                    atLeastJustAPair = any (\ num -> num == 1) (HM.values matchHowMany)
-                in
-                    isAtLeastEqual && atLeastJustAPair
-            Nothing -> false
+-- Newtypes
+newtype Orbit
+  = Orbit String
+
+instance showOrbit :: Show Orbit where
+  show (Orbit str) = str
+
+derive instance ordOrbit :: Ord Orbit
+
+derive instance eqOrbit :: Eq Orbit
+
+-- Type Aliases
+type Orbits
+  = Array (Tuple String String)
+
+splitOrbits :: String -> Orbits
+splitOrbits str =
+  let
+    lines = split (Pattern "\n") str
+
+    orbits = map (split (Pattern ")")) lines
+  in
+    map
+      ( \orbit -> case orbit of
+          [ orbited, orbiting ] -> Tuple orbiting orbited
+          _ -> Tuple "" ""
+      )
+      orbits
+
+reverseOrbits :: Orbits -> Orbits
+reverseOrbits orbits = map swap orbits
+
+getOutermostOrbits :: Orbits -> Set String
+getOutermostOrbits xs =
+  let
+    orbiting = fromFoldable $ map fst xs
+
+    orbited = fromFoldable $ map snd xs
+  in
+    difference orbiting orbited
+
+buildPath :: Orbits -> String -> Array String
+buildPath orbits orbiting =
+  let
+    orbitsMap = Map.fromFoldable orbits
+
+    nextOrbit = Map.lookup orbiting orbitsMap
+  in
+    case nextOrbit of
+      Just orbited -> orbiting : (buildPath orbits orbited)
+      Nothing -> [ orbiting ]
 
 main :: Effect Unit
-main =  
-  let nums = filter (\ num -> isEqualOrIncreasing $ splitNum num []) $ 136760 .. 595730 
-  in
-    log $ show $ length nums
-  
+main = do
+  text <- readTextFile UTF8 inputFile
+  let
+    orbits = splitOrbits text
+
+    outermost = getOutermostOrbits orbits
+
+    youPath = fromFoldable $ buildPath orbits "YOU"
+
+    sanPath = fromFoldable $ buildPath orbits "SAN"
+
+    sharedPathSize = size $ intersection youPath sanPath
+
+    -- take combined length of both paths
+    -- subtract 2x the shared path (at some point, the path from YOU intersects with the path from SAN, since both terminate at COM)
+    -- leaves the combined length of the distinct paths (subtract 2 to remove YOU and SAN orbits)
+    diff = (size youPath) + (size sanPath) - (2 * sharedPathSize) - 2
+  log $ show diff
