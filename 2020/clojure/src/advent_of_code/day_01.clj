d49a480dc230573841c0cc8ae23fe31a834a295f
Alex Dao <alex@urbint.com>
2020-12-02 23:34:06 -0500

    feat: day1
    Solved day1 pt1. Will return to day1 pt2 later

--- 2020/clojure/src/advent_of_code/day_01.clj
+++ 2020/clojure/src/advent_of_code/day_01.clj
@@ -1,11 +1,55 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]))
 
+(defn- check-sublists-pt1
+  [target [x & nums]]
+  (if-let [match (some #(when (= (- target x) %)
+                          %) nums)]
+      [x match]
+      (check-sublists-pt1 target nums)))
+      
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (let [nums (->> (str/split input #"\n")
+                  (map edn/read-string))
+        target 2020]
+    (apply * (check-sublists-pt1 target nums))))
+    
+
+(defn- check-sublists-pt2
+  [target nums]
+  (loop [target target
+         [x y & nums] nums]
+    (print x " " y "\n")
+   (if-let [match (and
+                   ;; ensure that the target is not smaller than sum of first 2 nums
+                   ;; (>= target (+ x y))
+                   (some #(when (= (- target x y) %)
+                            %)
+                         nums))]
+     [x y match]
+     (recur target (cons y nums)))))
+      
+    
+   
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (let [nums (->> (str/split input #"\n")
+                  (map edn/read-string))
+        target 2020]
+    (apply * (check-sublists-pt2 target nums))))
+
+
+
+(comment
+  (part-2 "979
+1721
+366
+299
+675
+1456
+"))
