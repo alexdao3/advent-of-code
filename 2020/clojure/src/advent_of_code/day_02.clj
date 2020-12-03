a73f25d66c22cf357d6924afd7fc8bcc1f9f0a30
Alex Dao <alex@urbint.com>
2020-12-02 23:34:25 -0500

    feat: day2, pt 1 & 2

--- 2020/clojure/src/advent_of_code/day_02.clj
+++ 2020/clojure/src/advent_of_code/day_02.clj
@@ -1,11 +1,41 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require [clojure.string :as str]))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (letfn [(check-valid-pw
+            [entry]
+            (let [[fst [letter] pw] (str/split entry #" ")
+                  [min-value max-value] (map read-string (str/split fst #"-"))
+                  letter-count (count (filter #(= letter %) pw))]
+              (<= min-value letter-count max-value)))]
+    (->> input
+         (filter check-valid-pw)
+         count)))
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (letfn [(check-valid-pw
+            [entry]
+            (let [[fst [letter] pw] (str/split entry #" ")
+                  pwd-indices (map (comp dec read-string) (str/split fst #"-"))
+                  valid-positions (filter #(= letter (nth pw %)) pwd-indices)]
+              (= 1 (count valid-positions))))]
+    (->> input
+         (filter check-valid-pw)
+         count)))
+
+(comment
+  (let [[fst [letter] pw]
+        (-> (slurp "resources/day-02.txt")
+            (str/split #"\n")
+            (first)
+            (str/split #" "))
+        [min-value max-value] (map read-string (str/split fst #"-"))]
+    (<= min-value (count (filter #(= letter %) pw)) max-value))
+
+  (-> (slurp "resources/day-02.txt")
+      (str/split #"\n")
+      (part-1)))
