585f662f619a528664bc1055acdbabb9d21d979a
Alex Dao <alex@urbint.com>
2020-12-06 01:38:13 -0500

    feat: day 6. dirty comment blocks

--- 2020/clojure/src/advent_of_code/day_06.clj
+++ 2020/clojure/src/advent_of_code/day_06.clj
@@ -1,4 +1,7 @@
-(ns advent-of-code.day-06)
+(ns advent-of-code.day-06
+  (:require
+   [clojure.string :as str] 
+   [clojure.set :as set]))
 
 (defn part-1
   "Day 06 Part 1"
@@ -9,3 +12,29 @@
   "Day 06 Part 2"
   [input]
   input)
+
+;; evil
+(def yes
+  #{\a \b \c \x \y \z})
+
+(def input
+  (-> (slurp "resources/day-06.txt")
+      (str/split #"\n\n")))
+
+(comment
+  ;; part 1
+  (->> (str/split input #"\n\n") 
+       (map #(str/split % #"\n"))
+       (map #(apply concat %)) 
+       (map set)
+       (map count)
+       (apply +))
+
+
+  ;; part 2
+  (->> (str/split input #"\n\n") 
+       (map #(str/split % #"\n"))
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map count)
+       (apply +)))
