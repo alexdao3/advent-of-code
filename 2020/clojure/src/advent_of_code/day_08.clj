ac98c0cedce3b98032101afe70c99e3497bf7fb9
Alex Dao <alex@urbint.com>
2020-12-08 01:30:41 -0500

    feat: day 08, pt 2

--- 2020/clojure/src/advent_of_code/day_08.clj
+++ 2020/clojure/src/advent_of_code/day_08.clj
@@ -44,6 +44,7 @@ acc +6")
       (update :visited #(conj % pos)))))
 
 (defn run-ops
+  "part 1"
   [instructions]
   (loop [{:keys [acc pos visited] :as return}
          {:acc 0
@@ -54,10 +55,76 @@ acc +6")
       (let [next (nth instructions pos)]
         (recur (process-instruction return next))))))
 
+(defn get-interchangeable
+  [instructions]
+  (reduce
+   (fn [acc [action dir-value]]
+     ;; (println acc action)
+     (if (#{"jmp" "nop"} action)
+       (-> acc
+           (update action #(conj % (:index acc)))
+           (update :index inc))
+       (update acc :index inc)))
+   {:index 0
+    "jmp" []
+    "nop" []}
+   instructions))
+
+(defn swap-instruction
+  [[action dir-value]]
+  (if (= "jmp" action)
+    ["nop" dir-value]
+    ["jmp" dir-value]))
+
+(defn run-ops-2
+  "part 2"
+  [instructions swap-pos]
+  (loop [{:keys [acc pos visited] :as return}
+         {:acc 0
+          :pos 0
+          :visited #{}}]
+    (cond (visited pos)
+          nil
+
+          (= pos (count instructions))
+          {:acc acc
+           :index pos}
+
+          :else (let [next (nth instructions pos)
+                      next (if (= swap-pos pos) (swap-instruction next) next)]
+                  (recur (process-instruction return next))))))
+
 (comment
+  ;; pt 1
   (->> input
        (map #(str/split % #" "))
        (run-ops))
+
+  ;; pt 2
+  (let [interchangeables (->> input
+                              (map #(str/split % #" "))
+                              (get-interchangeable))]
+    (println "SOLUTION?"
+             (->> input
+                  (map #(str/split % #" "))
+                  (#(some (fn [idx] (run-ops-2 % idx)) (get interchangeables "jmp"))))))
+
+
+  ;; pt 2
+
+
+  (let [interchangeables (->> input
+                              (map #(str/split % #" "))
+                              (get-interchangeable))]
+    (println "SOLUTION?"
+             (->> input
+                  (map #(str/split % #" "))
+                  (#(some (fn [idx] (run-ops-2 % idx)) (get interchangeables "nop"))))))
+
+  (->> input
+       (map #(str/split % #" "))
+       (#(some (fn [idx] (run-ops-2 % idx)) (get interchangeables "jmp"))))
+
   (first)
   (#(process-instruction {:acc 0
                           :pos 0
