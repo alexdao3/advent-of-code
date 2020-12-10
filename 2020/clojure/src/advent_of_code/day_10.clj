37f9edfb30222d4129ea589e82251128cf2c7438
Alex Dao <alex@urbint.com>
2020-12-10 00:18:44 -0500

    feat: day 10, pt1

--- 2020/clojure/src/advent_of_code/day_10.clj
+++ 2020/clojure/src/advent_of_code/day_10.clj
@@ -1,4 +1,6 @@
-(ns advent-of-code.day-10)
+(ns advent-of-code.day-10
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]))
 
 (defn part-1
   "Day 10 Part 1"
@@ -9,3 +11,25 @@
   "Day 10 Part 2"
   [input]
   input)
+
+(def example-input
+  (-> (slurp "resources/day-10.txt")
+      (str/split #"\n")))
+
+(comment
+  ;; pt 1
+  (let [nums (->> (map edn/read-string example-input)
+                  sort
+                  vec)
+        [first & rest] (concat [0] nums [(+ 3 (last nums))])]
+    (->>
+     (reduce (fn [{:keys [acc prev]} next]
+               {:acc (update acc (- next prev) (fnil inc 0))
+                :prev next})
+             {:acc {}
+              :prev first}
+             rest)
+     :acc
+     (map second)
+     (apply *))))
+     ;; (filter (comp #(> 0 %) first))
